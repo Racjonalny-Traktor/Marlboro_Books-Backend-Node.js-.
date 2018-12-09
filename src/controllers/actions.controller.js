1ee019b5d34072c6f2b4b6efae375c6e161dc435
Dawid Bartosz <dawid_bartosz@outlook.com>
2018-12-09 07:20:07 +0100

    feat(actions): add patch and delete routes, minor fixes

--- src/controllers/actions.controller.js
+++ src/controllers/actions.controller.js
@@ -1,20 +1,24 @@
 const BookSchema = require('../models/book.model');
 const ErrorHandler = require('../helpers/errors.helper');
 
+/**
+ * title-string
+ * desc-string
+ * author-string
+ * print-?string
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 const addBookToStore = async (req, res, next) => {
 
-    const { title, desc, author } = req.body;
+    const { title, desc, author, print } = req.body;
     let book;
 
-    if (req.body.print) {
-        const { print } = req.body;
-        book = new BookSchema({ title, desc, author, print });
-    }
+    book = print !== undefined ? new BookSchema({ title, desc, author, print }) : new BookSchema({ title, desc, author, print: null });
 
     try {
-        if (book) book = new BookSchema({ title, desc, author });
         await book.save();
-
         res.status(200).send({
             success: true,
             msg: `Added ${title} to store`
@@ -38,6 +42,56 @@ const getAllBooksData = async (req, res, next) => {
         console.error('[ERROR] Actions: Error occured while getting books from store')
         ErrorHandler.throwError(res, error);
     }
+    console.log('[SUCCESS] Actions: Returned all books from store');
 }
 
-module.exports = { addBookToStore, getAllBooksData };
+/**
+ * id-string
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+const removeBookFromStore = async (req, res, next) => {
+    const bookId = req.body.id;
+    console.log(bookId);
+    try {
+        await BookSchema.findOneAndDelete({ _id: bookId });
+        res.status(200).send({
+            success: true,
+            msg: 'Successfuly removed book from store'
+        });
+    } catch (error) {
+        console.error('[ERROR] Actions: Error occured while removing book from store')
+        ErrorHandler.throwError(res, error);
+    }
+    console.log('[SUCCESS] Actions: Removed book from store');
+}
+
+/**
+ * id-string
+ * data-object
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+const editBookData = async (req, res, next) => {
+    const bookId = req.body.id;
+    const updateData = req.body.data;
+    console.log(bookId);
+    console.log(updateData);
+    try {
+        await BookSchema.findOneAndUpdate({ _id: bookId }, updateData);
+        const data = await BookSchema.findOne({_id: bookId});
+        res.status(200).send({
+            success: true,
+            msg: 'Successfuly updated book in store',
+            update: data
+        });
+    } catch (error) {
+        console.error('[ERROR] Actions: Error occured while editing book in store')
+        ErrorHandler.throwError(res, error);
+    }
+    console.log('[SUCCESS] Actions: Edited book in store');
+}
+
+module.exports = { addBookToStore, getAllBooksData, removeBookFromStore, editBookData };
--- src/routes/actions.route.js
+++ src/routes/actions.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const ActionsController = require('../controllers/actions.controller');
 
 router.get('/book', ActionsController.getAllBooksData);
+router.delete('/book', ActionsController.removeBookFromStore);
 router.post('/book', ActionsController.addBookToStore);
+router.patch('/book', ActionsController.editBookData);
 
 module.exports = router;
